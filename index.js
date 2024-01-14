9c234d0d9594c53a5a157ee4573694d03b1a07a5
fauzanHI2023 <fauzan.aldi@human-initiative.org>
2024-01-14 18:41:06 +0700

    ganti file certificate7

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ const generatePDF = async (name, certificateNumber) => {
 
   firstPage.drawText(`${formattedDate}-00${certificateNumber}`, {
     x: 110,
-    y: 860,
+    y: 864,
     size: 15,
     font: SanChezFont,
     color: rgb(1, 1, 1),
